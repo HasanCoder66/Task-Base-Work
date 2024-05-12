dbcc40426862e15d3c7510761c8eba607f5bd10c
HasanCoder66 <hasanashraf874@gmail.com>
2024-05-12 16:18:30 +0500

    sweet alert

---
+++ Frontend/src/Components/Delete.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Delete = () => {
+  return (
+    <div>
+        swal({
+  title: "Are you sure?",
+  text: "You will not be able to recover this imaginary file!",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Yes, delete it!",
+  closeOnConfirm: false
+},
+function(){
+  swal("Deleted!", "Your imaginary file has been deleted.", "success");
+});
+    </div>
+  )
+}
+
+export default Delete
--- Frontend/src/Components/datatable/DataTable.jsx
+++ Frontend/src/Components/datatable/DataTable.jsx
@@ -3,25 +3,9 @@ import axios from 'axios';
 import { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'Image', headerName: 'Profile Image', width: 130 },
-    { field: 'name', headerName: 'Student name', width: 130 },
-    { field: 'courseName', headerName: 'Course name', width: 130 },
-    { field: 'password', headerName: 'Password', width: 130 },
-];
 
-// const rows = [
-//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
+
+
 
 
 export default function DataTable () {
